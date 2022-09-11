d48a5de5f4a8f42df3bbf24d4a88f058556dfa39
yasabee03 <yasmine_abdelaziz@brown.edu>
2022-09-11 17:46:07 -0400

    iu

--- app.js
+++ app.js
@@ -26,14 +26,24 @@ const updatePage = async () => {
 
   // Make API request and get an array of fruit objects
   const fruitsArray = await apiRequest();
-  // console.log(fruitsArray);
+  console.log(fruitsArray);
 
   // TODO: Use either `map` and/or `filter` to extract some data from the array of fruit objects
   // For example, find "name of all fruits whose sugar > 15",
+      const filteredArray = fruitsArray.filter((item) => {
+         return item.nutritions.sugar> 5;
+     });
 
-  // TODO: Create a new HTML element to display your data
-
+  // // TODO: Create a new HTML element to display your data
+   const elementA = document.createElement('div');
+ 
   // TODO: Append your new element to the page
+  const existingElement = document.getElementById('cs1300-gallery');
+  existingElement.append(elementA);
+  elementA.innerHTML = "Fruits with sugar > 5" + "<br>";
+  for(let i = 0; i<filteredArray.length; i++){
+    elementA.innerHTML += filteredArray[i].name + " " + "<br>";
+  }
 
 }
 
